5c4221bbf3ec1ed85bdd1b55ad9342282ae8f4f9
Marc Ripley <marcripley@gmail.com>
2014-03-19 09:30:47 -0700

    Initial Commit

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE HTML>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>Fixtures Library | Mission Bell</title>
+<!-- jQuery //-->
+<link rel="stylesheet" href="http://missionbell.com/js/slider/css/basic-jquery-slider.css" />
+<script src="http://missionbell.com/js/slider/js/libs/jquery-1.6.2.min.js"></script>
+<!-- HoverIntent and Basic jQuery Slider //-->
+<script src="http://missionbell.com/js/jquery.hoverIntent.minified.js"></script>
+<script src="http://missionbell.com/js/slider/js/basic-jquery-slider.js"></script>
+<!-- Shadowbox with jQuery //-->
+<link rel="stylesheet" type="text/css" href="http://missionbell.com/js/shadowbox/shadowbox.css">
+<script type="text/javascript" src="http://missionbell.com/js/shadowbox/shadowbox.js"></script>
+<!-- jQuery Validation plugin //-->
+<script type="text/javascript" src="http://missionbell.com/js/validation/dist/jquery.validate.js"></script>
+<!-- MB CSS and JS //-->
+<!--<link rel="stylesheet" type="text/css" href="http://missionbell.com/css/style.css" />-->
+<link rel="stylesheet" type="text/css" href="fixlib.css" />
+<link rel="icon" type="image/png" href="http://missionbell.com/images/favicon.png">
+<script type="text/javascript" src="http://missionbell.com/js/mb.js"></script>
+<script type="text/javascript" src="fixlib.js"></script>
+<!--[if IE]>
+	<link rel="stylesheet" type="text/css" href="http://missionbell.com/css/ie.css" />
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+<body>
+
+<header>
+	<div id="branding">
+		<hgroup id="logo"><h1 id="missionBell"><a href="index.php"><span>Mission Bell Manufacturing</span></a></h1></hgroup>
+		<hgroup id="tagline">
+			<h2><a href="index.php"><span>Working Wonders in Workspace</span><br />
+			Architectual Millwork,<br />
+			Woodwork and<br />
+			Casework</a></h2>
+		</hgroup>
+	
+		<div id="fixturesTitle">
+			<h1>Fixtures <span>Library</span></h1>
+		</div>
+	</div>	
+
+	<div id="fixturesFilter">
+		<form name="fixturesSelect" action="index.php" method="post" id="fixturesSelect">
+			<div class="fixturesFormDrop">
+				<label for="recipient">CATEGORY</label>
+				<select name="recipient" class="fixturesInput">
+					<option value="general">Desk</option>
+					<option value="sales">Paneling</option>
+					<option value="pm">Window Frame</option>
+					<option value="purch">Bar Counter</option>
+					<option value="acct">Bench</option>
+				</select>
+			</div>
+			<div class="fixturesFormDrop">
+				<label for="recipient">SUBCATEGORY</label>
+				<select name="recipient" class="fixturesInput">
+					<option value="general">Reception</option>
+					<option value="sales">Service Counter</option>
+					<option value="pm">Bank Fixture</option>
+					<option value="purch">Nurse Station</option>
+					<option value="acct">Circular</option>
+				</select>
+			</div>
+			<div class="fixturesTextInput">
+				<label for="recipient">TAGS</label>
+				<div class="fixturesFormItem">
+					<input type="text" name="tags" value="" class="fixturesInput" />
+				</div>
+				<a href="fixturesSeeAll">See All Tags</a>
+			</div>
+			<div class="fixturesTextInput">
+				<label for="recipient">JOB NUMBER</label>
+				<div class="fixturesFormItem">
+					<input type="text" name="tags" value="" class="fixturesInput" />
+				</div>
+				<a href="fixturesSeeAll">See All Jobs</a>
+			</div>
+			<!-- Additional search capability to be added
+			<div class="fixturesTextInput">
+				<label for="recipient">DATE RANGE</label>
+				<div class="fixturesFormItem">
+					<input type="text" name="tags" value="" class="fixturesInput" />
+				</div>
+				<a href="fixturesSeeAll">See All Jobs</a>
+			</div>
+			-->
+		</form>
+		<div class="clear"></div>
+	</div>
+
+</header>
+
+<div class="fixturesMain">
+	<a href="single.php"><div class="fixturesProj"><img src="http://missionbell.com/projects/c3-energy/images/c3-energy7-940x450.jpg" data-other-src="images/c3energySD.jpg" alt="Fixtures Library Project" width="940" height="450" class="flFeaturedImage" title="C3 Energy | Other, Planter, Shingles, Round" /><span class="fixturesProjTitle"><p></p></span></div></a>
+	<div class="fixturesProj"><img src="http://missionbell.com/projects/stanford-annenberg/images/stanford-annenberg3-940x450.jpg" data-other-src="images/stanfordSD.jpg" alt="Fixtures Library Project" width="940" height="450" class="flFeaturedImage" title="Stanford Annenberg | Table, Wood, Round" /><span class="fixturesProjTitle"><p></p></span></div>
+	<div class="fixturesProj"><img src="http://missionbell.com/projects/twitter/images/twitter2-940x450.jpg" data-other-src="images/twitterSD.jpg" alt="Fixtures Library Project" width="940" height="450" class="flFeaturedImage" title="Twitter | Wall, 3Form" /><span class="fixturesProjTitle"><p></p></span></div>
+	<div class="fixturesProj"><img src="http://missionbell.com/projects/passport/images/passport1-940x450.jpg" data-other-src="images/passportSD.jpg" alt="Fixtures Library Project" width="940" height="450" class="flFeaturedImage" title="Passport | Counter, Natural, Live Edge, Walnut" /><span class="fixturesProjTitle"><p></p></span></div>
+	<div class="fixturesProj"><img src="http://missionbell.com/projects/barnes/images/barnes8-940x450.jpg" data-other-src="images/stanfordSD.jpg" alt="Fixtures Library Project" width="940" height="450" class="flFeaturedImage" title="Barnes and Noble | Table, Long, White" /><span class="fixturesProjTitle"><p></p></span></div>
+	<div class="fixturesProj"><img src="http://missionbell.com/projects/kpmg-sv/images/kpmg-sv1-940x450.jpg" data-other-src="images/c3energySD.jpg" alt="Fixtures Library Project" width="940" height="450" class="flFeaturedImage" title="KPMG | The Source of Power and Energy" /><span class="fixturesProjTitle"><p></p></span></div>
+	<div class="fixturesProj"><img src="http://missionbell.com/projects/fvg/images/fvg2-940x450.jpg" data-other-src="images/passportSD.jpg" alt="Fixtures Library Project" width="940" height="450" class="flFeaturedImage" title="First Virtual Group | Hanging Wood Things, What Does It Mean" /><span class="fixturesProjTitle"><p></p></span></div>
+	<div class="fixturesProj"><img src="http://missionbell.com/projects/twitter/images/twitter6-940x450.jpg" data-other-src="images/twitterSD.jpg" alt="Fixtures Library Project" width="940" height="450" class="flFeaturedImage" title="Twitter Again | Break Room Cabinets, I Guess" /><span class="fixturesProjTitle"><p></p></span></div>
+	<div class="fixturesProj"><img src="http://missionbell.com/projects/level10/images/level102-940x450.jpg" data-other-src="images/c3energySD.jpg" alt="Fixtures Library Project" width="940" height="450" class="flFeaturedImage" title="Level 10 | Counter, Bar, Metal" /><span class="fixturesProjTitle"><p></p></span></div>
+	<div class="fixturesProj"><img src="http://missionbell.com/projects/google-mv/images/google-mv5-940x450.jpg" data-other-src="images/twitterSD.jpg" alt="Fixtures Library Project" width="940" height="450" class="flFeaturedImage" title="Google | A Googol is actually the number 10^100" /><span class="fixturesProjTitle"><p></p></span></div>
+	<div class="fixturesProj"><img src="http://missionbell.com/projects/frb/images/frb5-940x450.jpg" data-other-src="images/stanfordSD.jpg" alt="Fixtures Library Project" width="940" height="450" title="First Republic Bank | Stairs, Windy, Curvy Stairs" /><span class="fixturesProjTitle"><p></p></span></div>
+	<div class="fixturesProj"><img src="http://missionbell.com/projects/heller/images/heller1-940x450.jpg" data-other-src="images/passportSD.jpg" alt="Fixtures Library Project" width="940" height="450" class="flFeaturedImage" title="Heller Ehrman | Counter, Light Woodwork, Good Stuff" /><span class="fixturesProjTitle"><p></p></span></div>
+</div>
+
+</body>
+</html>
---
+++ single.php
@@ -0,0 +1,106 @@
+<!DOCTYPE HTML>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>Fixtures Library | Mission Bell</title>
+<!-- jQuery //-->
+<link rel="stylesheet" href="http://missionbell.com/js/slider/css/basic-jquery-slider.css" />
+<script src="http://missionbell.com/js/slider/js/libs/jquery-1.6.2.min.js"></script>
+<!-- HoverIntent and Basic jQuery Slider //-->
+<script src="http://missionbell.com/js/jquery.hoverIntent.minified.js"></script>
+<script src="http://missionbell.com/js/slider/js/basic-jquery-slider.js"></script>
+<!-- Shadowbox with jQuery //-->
+<link rel="stylesheet" type="text/css" href="http://missionbell.com/js/shadowbox/shadowbox.css">
+<script type="text/javascript" src="http://missionbell.com/js/shadowbox/shadowbox.js"></script>
+<!-- jQuery Validation plugin //-->
+<script type="text/javascript" src="http://missionbell.com/js/validation/dist/jquery.validate.js"></script>
+<!-- MB CSS and JS //-->
+<!--<link rel="stylesheet" type="text/css" href="http://missionbell.com/css/style.css" />-->
+<link rel="stylesheet" type="text/css" href="fixlib.css" />
+<link rel="icon" type="image/png" href="http://missionbell.com/images/favicon.png">
+<script type="text/javascript" src="http://missionbell.com/js/mb.js"></script>
+<script type="text/javascript" src="fixlib.js"></script>
+<!--[if IE]>
+	<link rel="stylesheet" type="text/css" href="http://missionbell.com/css/ie.css" />
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+<body>
+
+<header>
+	<div id="branding">
+		<hgroup id="logo"><h1 id="missionBell"><a href="http://missionbell.com/index.php"><span>Mission Bell Manufacturing</span></a></h1></hgroup>
+		<hgroup id="tagline">
+			<h2><a href="http://missionbell.com/index.php"><span>Working Wonders in Workspace</span><br />
+			Architectual Millwork,<br />
+			Woodwork and<br />
+			Casework</a></h2>
+		</hgroup>
+	
+		<div id="fixturesTitle">
+			<h1>Fixtures <span>Library</span></h1>
+		</div>
+	</div>
+	
+</header>
+
+<section>
+
+	<!-- The logic for pulling the project's photos and drawings loads here from a SQL query -->
+	<section class="flFeature">
+		<!-- Thumbnail-drive image carousel -->
+		<p>Click on an image to enlarge it</p>
+		<ul>
+			<li><a href="http://missionbell.com/projects/c3-energy/images/c3-energy7-940.jpg" rel="shadowbox[c3-energy]"><img src="http://missionbell.com/projects/c3-energy/images/c3-energy7-940x450.jpg" alt="C3 Energy" width="940" height="450" /></a></li>
+			<li><a href="http://missionbell.com/projects/c3-energy/images/c3-energy3-940.jpg" rel="shadowbox[c3-energy]"><img src="http://missionbell.com/projects/c3-energy/images/c3-energy3-940x450.jpg" alt="C3 Energy" width="940" height="450" /></a></li>
+			<li><a href="http://missionbell.com/projects/c3-energy/images/c3-energy2-940.jpg" rel="shadowbox[c3-energy]"><img src="http://missionbell.com/projects/c3-energy/images/c3-energy2-940x450.jpg" alt="C3 Energy" width="940" height="450" /></a></li>
+			<li><a href="http://missionbell.com/projects/c3-energy/images/c3-energy5-940.jpg" rel="shadowbox[c3-energy]"><img src="http://missionbell.com/projects/c3-energy/images/c3-energy5-940x450.jpg" alt="C3 Energy" width="940" height="450" /></a></li>
+			<li><a href="http://missionbell.com/projects/c3-energy/images/c3-energy4-940.jpg" rel="shadowbox[c3-energy]"><img src="http://missionbell.com/projects/c3-energy/images/c3-energy4-940x450.jpg" alt="C3 Energy" width="940" height="450" /></a></li>
+		</ul>
+	</section>
+
+	<section id="flDetailsSection">
+		<h2 id="flTitle"><!-- Job Title -->C3 Energy</h2>
+		<aside class="flDetails" id="flDetailsLabor">
+			<!-- This is the most vital portion of stats. It will pull the data initially from the MBdb on creation and fetch the most up-to-date data on query, but will also allow for manual edits. A version history and notification system will have to be put in place. -->
+			<h3>Labor Details</h3>
+			<ul>
+				<!-- Allow breakdown of departments -->
+				<li>Projected Hours: <span><!-- Dynamic -->700</span></li>
+				<li>Actual Hours: <span><!-- Dynamic -->550</span></li>
+		</aside>
+		<aside class="flDetails" id="flDetailsProject">
+			<h3>Project Details</h3>
+			<ul>
+				<li>Job Number: <span><strong><!-- Auto pull -->J12061</strong></span></li>
+				<li>Job Name: <span><!-- Auto pull -->C3 TI 5th Flr</span></li>
+				<li>Task Number: <span><strong><!-- Manual -->10, 23</strong></span></li>
+				<li>Job Location: <span><!-- Auto pull -->Redwood City</span></li>
+				<li>General Contractor: <span><!-- Auto pull -->Vance Brown</span></li>
+				<li>Architect: <span><!-- Auto pull -->Studio O+A</span></li>
+				<li>Job Close Date: <span><!-- Auto pull -->Dec. 3, 2012</span></li>
+			</ul>
+		</aside>
+		<aside class="flDetails" id="flDetailsOrg">
+			<h3>Organization</h3>
+			<ul>
+				<li>Category: <span><!-- Manual -->Decorative</span></li>
+				<li>Subcategory: <span><!-- Manual -->Planter</span></li>
+				<li>Tags: <!-- Tags loaded in from the db for quick viewing and editing. User MUST enter at least 3 tags to create a post. -->
+					<a href="#" class="post-tag">planter</a>
+					<a href="#" class="post-tag">shingles</a>
+					<a href="#" class="post-tag">wood</a>
+					<a href="#" class="post-tag">decorative</a>
+					<a href="#" class="post-tag">green</a>
+				</li>
+			</ul>
+		</aside>
+		<aside class="flDetails" id="flDetailsComments">
+			<h3>Comments</h2>
+			<p>Comments about the fixtures item go here, as well as any comments about the job.</p>
+		</aside>
+
+	</section>
+	
+</section>

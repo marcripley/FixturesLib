8dd8490909bd3ef09b477667b7bd7275cad5369a
timmissionbell <timm@missionbell.com>
2014-04-09 08:34:10 -0700

    Drop Down Lists
    The category drop down list auto populates from the database. It only
    populates with categories with a null parent value.
    
    Code has been laid out for the subcategory drop down list and the tags
    auto suggest field but it is not full functional yet.

--- Default.aspx
+++ Default.aspx
@@ -1,47 +1,47 @@
 ﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
     CodeFile="Default.aspx.cs" Inherits="_Default" %>
 
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="asp" %>
 <asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
-
 </asp:Content>
 <asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <form id="fixturesSelect" runat="server">
+    <div class="fixturesFormDrop">
+        <label for="recipient">
+            CATEGORY</label>
+        <asp:DropDownList ID="categoryList" runat="server" 
+            onselectedindexchanged="categoryList_SelectedIndexChanged" AutoPostBack="True" >
+        </asp:DropDownList>
+        <label for="recipient">
+            SUBCATEGORY</label>&nbsp;
+        <asp:DropDownList ID="subcategoryList" runat="server" Height="20px" OnSelectedIndexChanged="subcategoryList_SelectedIndexChanged"
+            Width="177px" DataTextField="name">
+        </asp:DropDownList>
+    </div>
+    <div class="fixturesTextInput">
+        <label for="recipient">
+            TAGS</label>
+        <div class="fixturesFormItem">
+            <asp:TextBox ID="tags" runat="server" Height="20px"></asp:TextBox>
+            &nbsp;</div>
+        <a href="fixturesSeeAll">See All Tags</a>
+    </div>
+    <div class="fixturesTextInput">
+        <label for="recipient">JOB NUMBER</label><asp:TextBox ID="jobNumber" runat="server"
+            Height="20px" OnTextChanged="jobNumberText_TextChanged"></asp:TextBox>
+        &nbsp;<div class="fixturesFormItem">
+            &nbsp;</div>
+        <a href="fixturesSeeAll">See All Jobs</a> &nbsp;</div>
+    <asp:Button ID="executeButton" runat="server" OnClick="executeButton_Click" Text="Execute Query"
+        Width="112px" />
+    <div class="clear">
 
-		<form id="fixturesSelect" runat="server">
-			<div class="fixturesFormDrop">
-				<label for="recipient">CATEGORY</label>
-                <asp:DropDownList ID="categoryList" runat="server" Height="20px" Width="175px">
-                </asp:DropDownList>
+        <asp:ToolkitScriptManager ID="ToolkitScriptManager1" runat="server">
+        </asp:ToolkitScriptManager>
 
-				<label for="recipient">SUBCATEGORY</label>&nbsp;
-                <asp:DropDownList ID="subcategoryList" runat="server" Height="20px" 
-                    onselectedindexchanged="subcategoryList_SelectedIndexChanged" 
-                    Width="177px">
-                </asp:DropDownList>
-			</div>
-
-			<div class="fixturesTextInput">
-				<label for="recipient">TAGS</label>
-				<div class="fixturesFormItem">
-					<asp:TextBox ID="tagsText" runat="server" Height="20px"></asp:TextBox>
-&nbsp;</div>
-				<a href="fixturesSeeAll">See All Tags</a>
-			</div>
-			<div class="fixturesTextInput">
-				J<label for="recipient">OB NUMBER</label><asp:TextBox ID="jobNumberText" 
-                    runat="server" Height="20px" ontextchanged="jobNumberText_TextChanged"></asp:TextBox>
-&nbsp;<div class="fixturesFormItem">
-					&nbsp;</div>
-				<a href="fixturesSeeAll">See All Jobs</a>
-			</div>
-			<div class="fixturesTextInput">
-				<label for="recipient">DATE RANGE</label>
-				<div class="fixturesFormItem">
-					</div>
-                <asp:TextBox ID="TextBox1" runat="server" Height="20px"></asp:TextBox>
-&nbsp;</div>
-		<asp:Button ID="Button1" runat="server" onclick="Button1_Click" 
-            Text="Execute Query" Width="112px" />
-        </form>
-		<div class="clear"></div>
-	</div>
+    <asp:AutoCompleteExtender ID="AutoCompleteExtender1" runat="server" TargetControlID="tags"
+         MinimumPrefixLength="1" EnableCaching="true" CompletionSetCount="1" CompletionInterval="1000"
+         ServiceMethod="GetCompletionList" />
+    </div>
+    </form>
 </asp:Content>
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -14,68 +14,166 @@ public partial class _Default : System.Web.UI.Page
 {
     string MBIntranet_DEV =
             ConfigurationManager.ConnectionStrings["MBIntranet_DEV"].ConnectionString;
-        protected void Page_Load(object sender, EventArgs e)
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        DataTable table = new DataTable();
+
+        var autoID = 1;
+        // get the connection
+
+        categoryList.Items.Clear();
+
+        using (SqlConnection conn = new SqlConnection(MBIntranet_DEV))
         {
-
-        }
-
-        protected void ExecuteQuery(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-            DataTable table = new DataTable();
-
-            var autoID = 1;
-            // get the connection
-
-            using (SqlConnection conn = new SqlConnection(MBIntranet_DEV))
+            // write the sql statement to execute
+            string sql = "SELECT ID, parent, name FROM flCategories";
+            // instantiate the command object to fire
+            using (SqlCommand cmd = new SqlCommand(sql, conn))
             {
-                // write the sql statement to execute
-                string sql = "SELECT TOP 5 jobNumber FROM flPosts";
-                // instantiate the command object to fire
-                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                // attach the parameter to pass if no parameter is in the sql no need to attach
+                SqlParameter prm = new SqlParameter("@autoId", autoID);
+                cmd.Parameters.Add(prm);
+                // get the adapter object and attach the command object to it
+                using (SqlDataAdapter ad = new SqlDataAdapter(cmd))
                 {
-                    // attach the parameter to pass if no parameter is in the sql no need to attach
-                    SqlParameter prm = new SqlParameter("@autoId", autoID);
-                    cmd.Parameters.Add(prm);
-                    // get the adapter object and attach the command object to it
-                    using (SqlDataAdapter ad = new SqlDataAdapter(cmd))
-                    {
-                        // fire Fill method to fetch the data and fill into DataTable
-                        ad.Fill(table);
-                    }
-                    // DataAdapter doesn't need open connection, it takes care of opening and closing the database connection
+                    // fire Fill method to fetch the data and fill into DataTable
+                    ad.Fill(table);
                 }
+                // DataAdapter doesn't need open connection, it takes care of opening and closing the database connection
             }
-            switch (categoryList.Text)
-            {
-                case "":
-                    break;
-                case " ":
-                    break;
-                default:
-                    break;
-            }
-            if (categoryList.Text == "")
-            {
 
-            }
-            // loop through the rows of the table
-            foreach (DataRow row in table.Rows)
-            {
-                Response.Write("<p>" + row["jobNumber"] + "</p>");
-            }
-            
         }
-        protected void subcategoryList_SelectedIndexChanged(object sender, EventArgs e)
+        // loop through the rows of the table and add the categories
+        foreach (DataRow row in table.Rows)
         {
-
+            if (string.IsNullOrEmpty(row["parent"].ToString()))
+            {
+                categoryList.Items.Add(row["name"].ToString());
+            }
         }
-        protected void jobNumberText_TextChanged(object sender, EventArgs e)
+
+        categoryList.Items.Insert(0, "Select");
+        categoryList.Items.Insert(1, "All");
+    }
+
+    public static List<string> GetCompletionList(string prefixText)
+    {
+        DataTable dt = new DataTable();
+        string constr = ConfigurationManager.ConnectionStrings["ConnectionString"].ToString();
+        SqlConnection con = new SqlConnection(constr);
+        con.Open();
+        SqlCommand cmd = new SqlCommand("select * from flPosts where tags like @Tag+'%'", con);
+        cmd.Parameters.AddWithValue("@Tag", prefixText);
+        SqlDataAdapter adp = new SqlDataAdapter(cmd);
+        adp.Fill(dt);
+        List<string> tags = new List<string>();
+        for (int i = 0; i < dt.Rows.Count; i++)
         {
+            tags.Add(dt.Rows[i][1].ToString());
+        }
+        return tags;
+    }
+
+    protected void executeButton_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void subcategoryList_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void jobNumberText_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+    
+    protected void categoryList_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        DataTable table = new DataTable();
+
+        int selectedIndex = categoryList.SelectedIndex-1;
+
+        string index = selectedIndex.ToString();
+
+        string idTemp = "";
+
+        //clear subcategory list before adding to it
+        subcategoryList.Items.Clear();
+
+        var autoID = 1;
+        // get the connection
+
+        // fill table with categories to find out if the category is a child of the category selected to
+        // display it as a subcategory in the subcategory down drop list
+        using (SqlConnection conn = new SqlConnection(MBIntranet_DEV))
+        {
+            // write the sql statement to execute
+            string sql = "SELECT ID, parent, name FROM flCategories";
+            // instantiate the command object to fire
+            using (SqlCommand cmd = new SqlCommand(sql, conn))
+            {
+                // attach the parameter to pass if no parameter is in the sql no need to attach
+                SqlParameter prm = new SqlParameter("@autoId", autoID);
+                cmd.Parameters.Add(prm);
+                // get the adapter object and attach the command object to it
+                using (SqlDataAdapter ad = new SqlDataAdapter(cmd))
+                {
+                    // fire Fill method to fetch the data and fill into DataTable
+                    ad.Fill(table);
+                }
+                // DataAdapter doesn't need open connection, it takes care of opening and closing the database connection
+            }
 
         }
+        // loop through the rows of the table
+
+        string selectedInd = "";
+
+        // find the ID of the currently selected Category and send to selectedInd
+        foreach (DataRow row in table.Rows)
+        {
+            if (categoryList.Text == row["name"].ToString());
+            {
+                selectedInd = row["ID"].ToString();
+            }
+        }
+
+        foreach (DataRow row in table.Rows)
+        {
+            if (selectedInd.Equals(row["name"].ToString()))
+            {
+                idTemp = row["ID"].ToString();
+            }
+        }
+
+        foreach (DataRow row in table.Rows)
+        {
+            if (idTemp == row["parent"].ToString())
+            {
+                subcategoryList.Items.Add(row["name"].ToString());
+            }
+        }
+    }
+
+
+    //code for auto completing tags
+    [System.Web.Services.WebMethodAttribute(), System.Web.Script.Services.ScriptMethodAttribute()]
+    public static string[] GetCompletionList(string prefixText, int count, string contextKey)
+    {
+        // Create array of movies  
+        string[] movies = { "Star Wars", "Star Trek", "Superman", "Memento", "Shrek", "Shrek II" };
+
+        // Return matching movies  
+        return (from m in movies where m.StartsWith(prefixText, StringComparison.CurrentCultureIgnoreCase) select m).Take(count).ToArray();
+
+    }
+
+    [System.Web.Services.WebMethodAttribute(), System.Web.Script.Services.ScriptMethodAttribute()]
+    public static string GetDynamicContent(string contextKey)
+    {
+        return default(string);
+    }
+
 }
